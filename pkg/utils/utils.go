987237436d2711dba780be3c1b7a754519f04a42
Roman Ivashkin <st086961@student.spbu.ru>
2024-03-06 11:11:57 +0300

    Add WithLocked utils

---
+++ pkg/utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+import "sync"
+
+func WithLocked[ReturnType any](lock *sync.Mutex, fu func() ReturnType) ReturnType {
+	lock.Lock()
+	defer lock.Unlock()
+	return fu()
+}
+
+func WithLockedNoResult(lock *sync.Mutex, fu func()) {
+	lock.Lock()
+	defer lock.Unlock()
+	fu()
+}
